6bc284cc8c5a2437031e44b6b9e4a98e12738e16
magiobus <aalbertobus@gmail.com>
2020-10-14 00:46:35 -0600

    Landing page seo

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,29 @@
 import Head from 'next/head'
 import { Flex, Heading, Text, Box, Button} from "@chakra-ui/core";
-import React, { useState, useEffect} from 'react';
+import { NextSeo } from 'next-seo';
 
 const Home = () => {
  
   return (
     <div className="container">
-      <Head>
-        <title>Korima Food </title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
+      <NextSeo
+      title="Korima Food 🌮 | Comida en Chihuahua México"
+      description="Encuentra lugares de comida en la ciudad de Chihuahua México"
+      canonical="https://www.korimafood.com/"
+      openGraph={{
+        url: 'https://www.korimafood.com/',
+        title: 'Korima Food 🌮',
+        description: 'Encuentra un lugar distinto para comer en la ciudad de Chihuahua',
+        images: [ {url: '/images/opengraph.jpg', width: 1280, height: 720, alt: 'Korima Food, Comida en Chihuahua'}],
+        site_name: 'Korima Food 🌮, lugares de comida en Chihuahua México',
+      }}
+      twitter={{ handle: '@magiobus', site: '@magiobus', cardType: 'summary_large_image'}}
+    />
+  
       <Flex align="center" justify="center" direction="column" mx={2} >
         <Box textAlign="center" bg="gray.50" borderWidth="1px" rounded="10px"  p={5}>
           <Heading as="h1" size={["2xl"]} color="red.500" mb={3}>Korima Food 🌮</Heading>
-          <Text textAlign="center" m={0} fontSize={["xl", "xl", "2xl"]} align="center">Encuentra un lugar distinto para comer en la ciudad de Chihuahua</Text>
+          <Text textAlign="center" m={0} fontSize={["lg", "xl", "2xl"]} align="center" color="gray.600"><b>Encuentra un lugar distinto para comer en la ciudad de Chihuahua</b></Text>
           <Button variantColor="red"  my={19}size={["lg"]}>Recomiendame un lugar</Button>
         </Box>
       </Flex>
@@ -28,7 +37,7 @@ const Home = () => {
           flex-direction: column;
           justify-content: center;
           align-items: center;
-          background-image: url('/backgrounds/background1.jpg');
+          background-image: url('/images/background.jpg');
           background-position: center;
           background-repeat: no-repeat;
           background-size: cover;
