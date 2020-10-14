9ed1a8f4d9d48088f4a9b67b394c4e8d58420a46
magiobus <aalbertobus@gmail.com>
2020-10-13 21:43:09 -0600

    Started app

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,22 @@
 import Head from 'next/head'
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Create Next App</title>
+        <title>Korima Food </title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
         <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href="https://nextjs.org">Korima Food</a>
         </h1>
 
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+        
 
+    
         <div className="grid">
           <a href="https://nextjs.org/docs" className="card">
             <h3>Documentation &rarr;</h3>
