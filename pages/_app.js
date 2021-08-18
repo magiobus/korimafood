9933a8ccb0b2b016b59b8bab88c01f1694c10289
magiobus <magio@magiobus.com>
2021-08-18 01:32:25 -0600

    Documentjs fixed and update to next11

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,12 @@
 import { ThemeProvider } from "@chakra-ui/core";
-import '../styles/sharebuttons.css';
+import "../styles/sharebuttons.css";
 
-
-
-const myApp = ({Component, pageProps}) => {
-return (
-    <ThemeProvider >
-        <Component {...pageProps} />
+const myApp = ({ Component, pageProps }) => {
+  return (
+    <ThemeProvider>
+      <Component {...pageProps} />
     </ThemeProvider>
-)
+  );
+};
 
-}
-
-export default myApp
+export default myApp;
